3a247af18d23dd19ad2c8c715a564a6572407773
Artjom Löbsack <ceigh@pm.me>
2020-08-06 22:16:45 +0500

    feat(project): initialize project

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: 'standard',
+  ignorePatterns: [
+    '!.commitlintrc.js',
+    '!.huskyrc.js',
+    '!.eslintrc.js',
+    '!.lintstagedrc.js'
+  ]
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# nuxt-chatra-module
